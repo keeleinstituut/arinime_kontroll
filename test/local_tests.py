b373d7f599e3ed6c7bad04b24c39f9400b8c55fc
silver <silvervapper@live.com>
2022-05-31 17:21:58 +0300

    New morpher script for all estnltk tasks, new conf files for various app and server settings, minor refactorings in main

--- test/local_tests.py
+++ test/local_tests.py
@@ -1,4 +1,9 @@
 from similarity.phonetic.phonetic_similarity import calculate_phon_sim
+from utils.morpher import get_lemma, tokenize, check_unknown
 
-res = calculate_phon_sim('tere')
-print(res)
+#res = calculate_phon_sim('tere')
+#print(res)
+
+print(get_lemma('kõik'))
+print(tokenize('Ilus part kolksus koos!'))
+print(check_unknown('frikadel'))
